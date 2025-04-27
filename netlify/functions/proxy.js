795cc87c893e94eb2346735c08bd687155b27de9
Ekaterina Bryksina <bryksinaekaterina2002@gmail.com>
2025-04-27 19:04:20 +0500

    ignore ssl

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,10 +1,21 @@
 const handler = async (event, context) => {
-    const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+    const fetch = (...args) =>
+      import('node-fetch').then(({ default: fetch }) => fetch(...args));
+  
+    const https = require('https');
+    const agent = new https.Agent({
+      rejectUnauthorized: false, // Игнорируем ошибки сертификатов
+    });
   
     const url = 'https://helppets.infy.uk/wp-json/wp/v2/posts';
   
     try {
-      const response = await fetch(url);
+      const response = await fetch(url, { agent });
+  
+      if (!response.ok) {
+        throw new Error(`Ошибка при получении данных: ${response.statusText}`);
+      }
+  
       const posts = await response.json();
   
       return {
@@ -12,6 +23,8 @@ const handler = async (event, context) => {
         body: JSON.stringify(posts),
       };
     } catch (error) {
+      console.error('Ошибка:', error.message);
+  
       return {
         statusCode: 500,
         body: JSON.stringify({ message: 'Error fetching posts', error: error.message }),
@@ -19,4 +32,4 @@ const handler = async (event, context) => {
     }
   };
   
-exports.handler = handler;
+  exports.handler = handler;
