b4b6f0436736bfb1fbb647ef8d32acbbf08c47cd
Ekaterina Bryksina <bryksinaekaterina2002@gmail.com>
2025-05-13 01:08:58 +0500

    wp

---
+++ script/wp.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('https://help-pets.uz/wp-json/wp/v2/pages?slug=welcome-page')
+      .then(response => response.json())
+      .then(pages => {
+        if (pages.length > 0) {
+          const welcomePage = pages[0];
+          const contentDiv = document.getElementById('wordpress-content');
+          contentDiv.innerHTML = `<h2>${welcomePage.title.rendered}</h2>${welcomePage.content.rendered}`;
+        } else {
+          const contentDiv = document.getElementById('wordpress-content');
+          contentDiv.innerText = 'Контент не найден.';
+        }
+      })
+      .catch(error => {
+        console.error('Ошибка при получении данных из WordPress:', error);
+        const contentDiv = document.getElementById('wordpress-content');
+        contentDiv.innerText = 'Произошла ошибка при загрузке контента.';
+      });
+  });
