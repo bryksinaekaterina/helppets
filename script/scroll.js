6e6290f971de42f3741a1c0bb2cf50efd98dca48
Ekaterina Bryksina <bryksinaekaterina2002@gmail.com>
2025-05-14 01:56:16 +0500

    footer-mobile

--- script/scroll.js
+++ script/scroll.js
@@ -1,9 +1,30 @@
-const scrollBtn = document.querySelector('#help-btn');
+const scrollContainer = document.querySelector('.news-scroll-container');
 
-function scrollToSection() {
-    document.getElementById("with-form").scrollIntoView({
-      behavior: "smooth"
-    });
-  }
+let isDown = false;
+let startX;
+let scrollLeft;
 
-scrollBtn.addEventListener('click', scrollToSection)  
+scrollContainer.addEventListener('mousedown', (e) => {
+    isDown = true;
+    scrollContainer.classList.add('active');
+    startX = e.pageX - scrollContainer.offsetLeft;
+    scrollLeft = scrollContainer.scrollLeft;
+});
+
+scrollContainer.addEventListener('mouseleave', () => {
+    isDown = false;
+    scrollContainer.classList.remove('active');
+});
+
+scrollContainer.addEventListener('mouseup', () => {
+    isDown = false;
+    scrollContainer.classList.remove('active');
+});
+
+scrollContainer.addEventListener('mousemove', (e) => {
+    if (!isDown) return; // Прекращаем выполнение функции, если кнопка мыши не нажата
+    e.preventDefault();
+    const x = e.pageX - scrollContainer.offsetLeft;
+    const walk = (x - startX) * 2; // Умножаем на 2 для увеличения скорости прокрутки
+    scrollContainer.scrollLeft = scrollLeft - walk;
+});
